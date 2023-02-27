9373a65e21d01192b309f9dde41eb0a5c80027a2
kuryer <kuryergdev@gmail.com>
2023-02-27 20:11:13 +0100

    27.02 - 2

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -2,22 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManagerScript : MonoBehaviour
 {
-    [SerializeField] CanvasScript canvasScript;
+    private static GameManagerScript instance;
 
-    private static GameObject instance;
+    [SerializeField] CanvasScript canvasScript;
 
     bool isPaused = false;
     bool canOpenPauseMenu;
+
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
 
         if (instance == null)
         {
-            instance = gameObject;
+            instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
@@ -27,18 +29,15 @@ public class GameManagerScript : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && canOpenPauseMenu)
         {
             TogglePauseMenu();
         }
     }
 
+    #region PauseMenu
     public void TogglePauseMenu()
     {
-        //Sprawdzam czy moge w ogóle w³¹czyæ Pause Menu
-        if (!canOpenPauseMenu)
-            return;
-
         //Sprawdzam czy ju¿ spauzowa³em czy jeszcze nie i manipuluje czasem w zale¿noœci od stanu Pause Menu
         if (!isPaused)
         {
@@ -63,6 +62,23 @@ public class GameManagerScript : MonoBehaviour
         Time.timeScale = 1;
         isPaused = false;
     }
+    #endregion
+
+    public async void LoadScene(int LevelIndex)
+    {
+        var scene = SceneManager.LoadSceneAsync(LevelIndex);
+        scene.allowSceneActivation = false;
+
+        //cast here animation to transition into loading screen
+
+        do
+        {
+            await System.Threading.Tasks.Task.Delay(100);
+        }while (scene.progress < .9f /* jeszcze bool odnosnie animacji*/);
+
+        scene.allowSceneActivation = true;
+
+    }
     public void LoadLevel(int LevelIndex)
     {
         SceneManager.LoadSceneAsync(LevelIndex);
@@ -76,4 +92,5 @@ public class GameManagerScript : MonoBehaviour
         canOpenPauseMenu = false;
         SetResume();
     }
+
 }
