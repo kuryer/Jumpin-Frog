e1ea7fc03fa9e74c1d0a6da9a1fbe1c571586038
kuryer <kuryergdev@gmail.com>
2023-02-27 21:08:07 +0100

    27.02 -3

---
+++ Assets/Scripts/UI Scripts/LevelTransitionScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTransitionScript : MonoBehaviour
+{
+    public void LevelTransitionAnimationClosed()
+    {
+        // wpisac zeby wyslal info o zmianie boola do game managera i zmienic te gowniana nazwe xdd
+    }
+}
