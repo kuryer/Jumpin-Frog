21773da2dce8df67b543fa82a8719984e72d4723
kuryer <kuryergdev@gmail.com>
2024-03-02 17:45:10 +0100

    animation state

---
+++ Assets/Scripting/Scriptable Objects/Player/Animations/AnimationState.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Scriptable Objects/Player/Animation State")]
+public class AnimationState : ScriptableObject
+{
+    public string AnimationName;
+}
