e41cc5bd746e17c550a564999002b29e1bef8699
kuryer <kuryergdev@gmail.com>
2024-03-23 18:10:14 +0100

    basic tracker + height bar funcionality

--- Assets/Scripting/Scripts/Camera/State Machine/CameraStateMachine.cs
+++ Assets/Scripting/Scripts/Camera/State Machine/CameraStateMachine.cs
@@ -3,10 +3,6 @@ using UnityEngine;
 
 public class CameraStateMachine : MonoBehaviour
 {
-    [Header("Player Tracking")]
-    [SerializeField] Transform playerTransform;
-    [SerializeField] TransformRuntimeValue trackedTransform;
-    [SerializeField] TransformRuntimeValue trackerTransform;
 
     [Header("State Management")]
     [SerializeField] CameraState InitState;
@@ -20,33 +16,19 @@ public class CameraStateMachine : MonoBehaviour
     void Initialize()
     {
         ActualState.Value = InitState;
-        ActualState.Value.OnEnter();
-        trackerTransform.SetItem(transform);
-        trackedTransform.SetItem(playerTransform);
-    }
-
-    void Update()
-    {
-        ActualState.Value.OnUpdate();
+        ChangeAnimationState(ActualState.Value.CameraAnimationState);
     }
 
     public void ChangeState(CameraState state)
     {
         if (ActualState.Value == state)
             return;
-        ActualState.Value.OnExit();
         ActualState.Value = state;
-        ActualState.Value.OnEnter();
+        ChangeAnimationState(ActualState.Value.CameraAnimationState);
     }
 
-    public void ChangeAnimationState(string animationName)
+    void ChangeAnimationState(string animationName)
     {
         animator.Play(animationName);
-    }
-
-    public void CMBrainEventGrabber(ICinemachineCamera cm1, ICinemachineCamera cm2)
-    {
-        if(cm1 != null)Debug.Log(cm1.Name);
-        if(cm2 != null)Debug.Log(cm2.Name);
     }
 }
