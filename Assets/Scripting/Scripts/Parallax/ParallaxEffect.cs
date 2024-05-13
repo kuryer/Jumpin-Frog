1ddce58d7ed7432fbb53132dbc603837297453d1
kuryer <kuryergdev@gmail.com>
2024-05-13 15:54:00 +0200

    chase Y damp

--- Assets/Scripting/Scripts/Parallax/ParallaxEffect.cs
+++ Assets/Scripting/Scripts/Parallax/ParallaxEffect.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class ParallaxEffect : MonoBehaviour
 {
     [SerializeField] TransformRuntimeValue referenceSubject;
+    [SerializeField] float addedHeight;
     [Range(0.01f,1f)]
-    [SerializeField] float parallaxValue;
+    [SerializeField] float parallaxValueY;
     [Range(0.01f, 1f)]
-    [SerializeField] float smoothTime;
+    [SerializeField] float parallaxValueX;
+
+    [Range(0.01f, 1f)]
+    [SerializeField] float smoothTimeY;
+    [Range(0.01f, 1f)]
+    [SerializeField] float smoothTimeX;
     Vector3 startPosition;
     Vector3 velocity;
-    Vector3 travel => Vector2.right * (referenceSubject.Item.position - startPosition);
+    Vector3 travel => /*Vector2.right **/ (referenceSubject.Item.position - startPosition) + new Vector3(0, addedHeight, 0);
 
 
     void Start()
@@ -21,11 +27,14 @@ public class ParallaxEffect : MonoBehaviour
 
     void Update()
     {
-        // nah stillo potrzebny damping na X ale oddzielny
         Vector3 YPosition = new Vector3(0,transform.position.y, 0);
-        Vector3 YDifference = new Vector3(0, startPosition.y + travel.y * parallaxValue); 
-        float YDamp = Vector3.SmoothDamp(YPosition, YDifference, ref velocity, smoothTime).y;
-        float XPos = startPosition.x + travel.x * parallaxValue;
-        transform.position = new Vector3(XPos, YDamp, transform.position.z);
+        Vector3 YDifference = new Vector3(0, startPosition.y + travel.y * parallaxValueY); 
+        float YDamp = Vector3.SmoothDamp(YPosition, YDifference, ref velocity, smoothTimeY).y;
+
+
+        Vector3 XPosition = new Vector3(transform.position.x, 0,0);
+        Vector3 XDifference = new Vector3(startPosition.x + travel.x * parallaxValueX, 0,0);
+        float XDamp = Vector3.SmoothDamp(XPosition, XDifference, ref velocity, smoothTimeX).x;
+        transform.position = new Vector3(XDamp, YDamp, transform.position.z);
     }
 }
